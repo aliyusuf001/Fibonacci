d7926c7e57827e29e6b85ca8100947d4f13f2759
DarkAce01 <quadrikazeem01@gmail.com>
2019-10-04 15:52:49 +0100

    fibonacci first 1000

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong count, firstNumber = 0, secondNumber = 1, nestNumber;
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(secondNumber);
+            for (count = 2; count < 1000; count++)
+            {
+                nestNumber = firstNumber + secondNumber;
+                Console.WriteLine(nestNumber);
+
+                firstNumber = secondNumber;
+                secondNumber = nestNumber;
+            }
+            Console.ReadLine();
+        }
+    }
+}
